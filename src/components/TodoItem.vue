6979f06b1484f5db2d3e680d0c0c102bd9e5df2c
Everypossible <eee1755@outlook.com>
2021-09-24 20:07:56 +0800

    (美化样式)增大表单项todoItem前面的勾选框的大小

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -83,41 +83,7 @@ export default {
 </script>
 
 <style scoped>
-* {
-  margin: 0;
-  padding: 0;
-}
-html,
-body {
-  width: 100%;
-  height: 100%;
-}
-body {
-  background: linear-gradient(135deg, #eebd89, #d13abd);
-}
-.container {
-  margin: 100px 0 0 100px;
-  width: 300px;
-  padding: 0 30px;
-  border: 1px solid #eee;
-  border-radius: 4px;
-  background-color: #efefef;
-  box-shadow: 6px 2px 5px rgba(0, 0, 0, 0.2);
-}
-
-.todo-list-title {
-  color: #333;
-  text-shadow: 4px 2px 2px rgba(0, 0, 0, 0.2);
-  text-align: center;
-}
-
-ul {
-  list-style: none;
-}
-.todo-list {
-  margin-top: 10px;
-}
-.todo-list .todo-item {
+.todo-item {
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -128,39 +94,11 @@ ul {
   background-color: #fff;
   font-size: 14px;
 }
-.no-todo-List {
-  margin-top: 40px;
-  text-align: center;
-  color: #999;
-  font-size: 14px;
-}
 .todo-item.finish {
   text-decoration: line-through;
 }
 
-.add-todo-container {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin: 14px 0;
-}
-
-.add-todo-container input {
-  flex: 1;
-  margin-left: 10px;
-  margin-right: 12px;
-  line-height: 32px;
-  border: none;
-  outline: none;
-  border-radius: 4px;
-}
-.add-todo-container .add-btn {
-  display: inline-block;
-  padding: 0 18px;
-  border-radius: 4px;
-  line-height: 32px;
-  color: #fff;
-  background: #1989fa;
-  cursor: pointer;
+input[type="checkbox"]{
+  zoom:180%;
 }
 </style>
